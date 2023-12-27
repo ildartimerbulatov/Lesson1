2b1acadc73a330ee8f73274ecf8590085f01822f
ildar <ildar@example.com>
2023-12-27 21:34:25 +0300

    вывод задача кратности 7 и 23

---
+++ Task6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Запрос числа у пользователя
+        Console.Write("Введите число: ");
+
+        // Считывание введенного числа
+        string input = Console.ReadLine();
+
+        // Попытка преобразовать введенную строку в целое число
+        if (int.TryParse(input, out int number))
+        {
+            // Проверка, кратно ли введенное число одновременно 7 и 23
+            if (IsMultipleOf7And23(number))
+            {
+                Console.WriteLine($"{number} кратно 7 и 23 одновременно.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} не кратно 7 и 23 одновременно.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Неверный ввод. Введите целое число.");
+        }
+    }
+
+    // Функция для проверки, кратно ли число одновременно 7 и 23
+    static bool IsMultipleOf7And23(int number)
+    {
+        return (number % 7 == 0) && (number % 23 == 0);
+    }
+}
